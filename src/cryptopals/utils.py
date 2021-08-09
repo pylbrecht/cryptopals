4294004f17794e2236edbb836f0044004e7ae422
pylbrecht <pylbrecht@gmail.com>
2021-08-09 22:49:35 +0200

    Add decrypt() and score() to utils

--- src/cryptopals/utils.py
+++ src/cryptopals/utils.py
@@ -10,3 +10,18 @@ def xor(a: str, b: str) -> str:
     return bytearray(
         a ^ b for a, b in zip(bytearray.fromhex(a), bytearray.fromhex(b))
     ).hex()
+
+
+def score(text: str) -> int:
+    score = 0
+    for letter in text:
+        if letter.upper() not in "ETAOIN SHRDLU":
+            continue
+
+        score += 1
+
+    return score
+
+
+def decrypt(cipher: bytes, key: int) -> bytes:
+    return bytearray(c ^ key for c in bytearray.fromhex(cipher))
