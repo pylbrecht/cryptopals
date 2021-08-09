e0e87edce588d38a1a09d6bd0631b19b84e75717
pylbrecht <pylbrecht@gmail.com>
2021-08-09 20:55:11 +0200

    Add test for utils.hex_to_base64()

---
+++ tests/test_utils.py
@@ -0,0 +1,7 @@
+from cryptopals import utils
+
+
+def test_hex_to_base64():
+    hex_ = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"  # noqa: E501
+    b64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    assert utils.hex_to_base64(hex_) == b64
